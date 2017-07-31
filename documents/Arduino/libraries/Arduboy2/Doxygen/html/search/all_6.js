61208171261ba3c8288797fad474fa4263db5aec
Scott Allen <saydisp-git@yahoo.ca>
2017-07-31 17:40:18 -0400

    Update Arduboy2 documentation to v4.1.0

--- documents/Arduino/libraries/Arduboy2/Doxygen/html/search/all_6.js
+++ documents/Arduino/libraries/Arduboy2/Doxygen/html/search/all_6.js
@@ -4,6 +4,10 @@ var searchData=
   ['getcursorx',['getCursorX',['../classArduboy2.html#ac645a477351f23e4544aed88dcdae007',1,'Arduboy2']]],
   ['getcursory',['getCursorY',['../classArduboy2.html#afc291e1f45319aca8c7d932b5864b134',1,'Arduboy2']]],
   ['getpixel',['getPixel',['../classArduboy2Base.html#a87c8fc7f1de897e3f3f863a721e55e39',1,'Arduboy2Base']]],
+  ['gettextbackground',['getTextBackground',['../classArduboy2.html#aafde2dcbb335d5c35ca737304b9f0346',1,'Arduboy2']]],
+  ['gettextcolor',['getTextColor',['../classArduboy2.html#abacdc986338518804fb8e6e745204b5f',1,'Arduboy2']]],
+  ['gettextsize',['getTextSize',['../classArduboy2.html#a2c3794841eef317c92ca74a3b471092f',1,'Arduboy2']]],
+  ['gettextwrap',['getTextWrap',['../classArduboy2.html#ab3b509ea3003caa541faad483261cf6c',1,'Arduboy2']]],
   ['glcdfont_2ec',['glcdfont.c',['../glcdfont_8c.html',1,'']]],
   ['green_5fled',['GREEN_LED',['../Arduboy2Core_8h.html#a01649d652fa50957c6ef3c32b1238038',1,'Arduboy2Core.h']]]
 ];
--- documents/Arduino/libraries/Arduboy2/Doxygen/html/search/functions_6.js
+++ documents/Arduino/libraries/Arduboy2/Doxygen/html/search/functions_6.js
@@ -3,5 +3,9 @@ var searchData=
   ['getbuffer',['getBuffer',['../classArduboy2Base.html#aacd29034dd9dbd15e0906924c9e3e379',1,'Arduboy2Base']]],
   ['getcursorx',['getCursorX',['../classArduboy2.html#ac645a477351f23e4544aed88dcdae007',1,'Arduboy2']]],
   ['getcursory',['getCursorY',['../classArduboy2.html#afc291e1f45319aca8c7d932b5864b134',1,'Arduboy2']]],
-  ['getpixel',['getPixel',['../classArduboy2Base.html#a87c8fc7f1de897e3f3f863a721e55e39',1,'Arduboy2Base']]]
+  ['getpixel',['getPixel',['../classArduboy2Base.html#a87c8fc7f1de897e3f3f863a721e55e39',1,'Arduboy2Base']]],
+  ['gettextbackground',['getTextBackground',['../classArduboy2.html#aafde2dcbb335d5c35ca737304b9f0346',1,'Arduboy2']]],
+  ['gettextcolor',['getTextColor',['../classArduboy2.html#abacdc986338518804fb8e6e745204b5f',1,'Arduboy2']]],
+  ['gettextsize',['getTextSize',['../classArduboy2.html#a2c3794841eef317c92ca74a3b471092f',1,'Arduboy2']]],
+  ['gettextwrap',['getTextWrap',['../classArduboy2.html#ab3b509ea3003caa541faad483261cf6c',1,'Arduboy2']]]
 ];
