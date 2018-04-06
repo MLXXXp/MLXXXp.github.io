812e34e8115913ff0d5cbaa858935fcc411329d0
Scott Allen <saydisp-git@yahoo.ca>
2018-04-06 15:50:50 -0400

    Update Arduboy2 documentation to v5.1.0

--- documents/Arduino/libraries/Arduboy2/Doxygen/html/search/functions_11.js
+++ documents/Arduino/libraries/Arduboy2/Doxygen/html/search/functions_11.js
@@ -4,6 +4,7 @@ var searchData=
   ['width',['width',['../classArduboy2Core.html#a24db7ae688ae9cc5a83aaf28a16a723d',1,'Arduboy2Core']]],
   ['write',['write',['../classArduboy2.html#adc1b073783b71ae14843a4a0e806289a',1,'Arduboy2']]],
   ['writeshowbootlogoflag',['writeShowBootLogoFlag',['../classArduboy2Base.html#a83a52e254e92eb210979ca9fe139dd2e',1,'Arduboy2Base']]],
+  ['writeshowbootlogoledsflag',['writeShowBootLogoLEDsFlag',['../classArduboy2Base.html#a1752f9cd34722b26eb9f74daf4d93d1c',1,'Arduboy2Base']]],
   ['writeshowunitnameflag',['writeShowUnitNameFlag',['../classArduboy2Base.html#a121a9276b05c2d206fddb932f2ca98da',1,'Arduboy2Base']]],
   ['writeunitid',['writeUnitID',['../classArduboy2Base.html#a1947d7386a7800643d73743f8ce00f41',1,'Arduboy2Base']]],
   ['writeunitname',['writeUnitName',['../classArduboy2Base.html#a8d63d2fc0f45ab6af82616bbe6422785',1,'Arduboy2Base']]]
